9923df9a46faf537d12934db10361cbf88778c45
zloid <zloid-1@yandex.by>
2021-02-01 18:00:05 +0100

    start: project

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import { Map } from '../components/Map/Map'
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            weather now...??
+            {/* <Map /> */}
+        </div>
+    )
+}
+
+export default App

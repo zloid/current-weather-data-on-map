05e5485ea4cb1be83f7944835b136296bf33a0a3
zloid <zloid-1@yandex.by>
2021-02-03 11:26:37 +0100

    fix: mapOfWeatherSlice.js

--- src/features/mapOfWeather/mapOfWeatherSlice.js
+++ src/features/mapOfWeather/mapOfWeatherSlice.js
@@ -4,7 +4,6 @@ const initialState = {
     loading: false,
     hasErrors: false,
     weatherData: [],
-    someData: '',
     resultDataForMap: [],
 }
 
@@ -24,12 +23,10 @@ const mapOfWeatherSlice = createSlice({
             state.loading = false
             state.hasErrors = true
         },
-        pullResultDataForMap(state, { payload }) {
-            state.resultDataForMap.push(payload)
+        pullForwardGeoDataForMap(state, { payload }) {
+            state.resultDataForMap = payload
         },
-        someReducer(state, { payload }) {
-            state.someData = payload
-        },
+        getFinalForwardGeoDataAndTemperature() {},
     },
 })
 
@@ -41,14 +38,14 @@ export const {
     getWeather,
     getWeatherSuccess,
     getWeatherFailure,
-    pullResultDataForMap,
-    someReducer,
+    pullForwardGeoDataForMap,
+    getFinalForwardGeoDataAndTemperature,
 } = mapOfWeatherSlice.actions
 
-// reducer
+// main reducer
 export default mapOfWeatherSlice.reducer
 
-// Asynchronous thunk action
+// asynchronous thunk action
 export function fetchWeather() {
     return async (dispatch) => {
         dispatch(getWeather())
@@ -60,16 +57,12 @@ export function fetchWeather() {
             )
             const allStationWithWeather = await response.json()
 
-            // dispatch(getWeatherSuccess(allStationWithWeather))
+            dispatch(getWeatherSuccess(allStationWithWeather))
 
-            console.log(
-                'allStationWithWeather',
-                allStationWithWeather[0].stacja
-            )
-
+            // forward geocoding by city name
             let promisesOfForwardGeo = []
 
-            for (let i = 0; i < 20; i++) {
+            for (let i = 0; i < 1; i++) {
                 promisesOfForwardGeo.push(
                     fetch(
                         `http://api.positionstack.com/v1/forward?access_key=358c451c8bc4c40048fd777aa721ad30&query=1600%${allStationWithWeather[i].stacja}`
@@ -78,89 +71,21 @@ export function fetchWeather() {
             }
 
             promisesOfForwardGeo = promisesOfForwardGeo.map((promise, key) =>
-                promise.then((resolve) => resolve.json()).then((geoData) => {
-                    console.log('key', key)
-                    return geoData.data[0]
-                })
+                promise
+                    .then((resolve) => resolve.json())
+                    .then((geoData) => {
+                        console.log('key', key)
+                        return geoData.data[0]
+                    })
             )
 
             Promise.all(promisesOfForwardGeo)
                 .then((response) => {
                     console.log(response)
 
-                    dispatch(getWeatherSuccess(response))
+                    dispatch(pullForwardGeoDataForMap(response))
                 })
-                .catch((error) => alert(error))
-
-            /* 
-            promisesOfForwardGeo = promisesOfForwardGeo.map((e) =>
-                e
-                    .then((resolve) => resolve.json())
-                    .then((data) => data.results[0].name)
-            )
-
-            Promise.all(promisesOfForwardGeo)
-                .then((resolve) => {
-                    console.log(resolve)
-                    resolve.map((e) => document.write(e.last, ', '))
-                })
-                .catch((e) => alert('reloadddd!')) */
-
-            /* 
-            const randomStacja =
-                allStationWithWeather[
-                    Math.floor(Math.random() * allStationWithWeather.length)
-                ].stacja
-
-            console.log(randomStacja, 'adfsasdfasdf') */
-
-            // forward geocoding
-            /* 
-            const getForwardGeocoding = await fetch(
-                `http://api.positionstack.com/v1/forward?access_key=358c451c8bc4c40048fd777aa721ad30&query=1600%${randomStacja}`
-            )
-            const getForwardGeocodJSON = await getForwardGeocoding.json()
-            const resultForwardGeocodForMap = getForwardGeocodJSON.data[0].name
-            console.log('resultForwardGeocodForMap', resultForwardGeocodForMap)
- */
-            /* 
-            let promisesOfForwardGeo = []
-            for (let i = 0; i < 0; i++) {
-                const randomStacja =
-                    allStationWithWeather[
-                        Math.floor(Math.random() * allStationWithWeather.length)
-                    ].stacja
-
-                promisesOfForwardGeo.push(
-                    fetch(
-                        `http://api.positionstack.com/v1/forward?access_key=358c451c8bc4c40048fd777aa721ad30&query=1600%${randomStacja}`
-                    )
-                )
-            }
-
-              */
-
-            /* todo */
-            /* 
-            const responseSome = await fetch('https://randomuser.me/api/')
-            const someData = await responseSome.json()
-
-            const someResult = someData.results[0].name.first
-
-            console.log(someResult)
-            dispatch(
-                someReducer(
-                    resultForwardGeocodForMap +
-                        '___' +
-                        someResult +
-                        ' : ' +
-                        allStationWithWeather[
-                            Math.floor(
-                                Math.random() * allStationWithWeather.length
-                            )
-                        ].temperatura
-                )
-            ) */
+                .catch(() => alert('Some problem, please reload page!'))
         } catch (error) {
             dispatch(getWeatherFailure())
             alert('Some problem, please reload page!')
